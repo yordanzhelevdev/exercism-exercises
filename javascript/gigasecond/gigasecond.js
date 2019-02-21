f795f96232f306aab951647ccf40c36ceed41f9b
yordanzhelevdev <yordanzhelevdev@gmail.com>
2019-02-22 00:32:30 +0200

    Simplified my solution

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,7 +1,1 @@
-export const gigasecond = (date) => { 
-    const dateMs = date.getTime() / 1000;
-    const gigasecond = Math.pow(10, 9);
-    const timeInMs = dateMs + gigasecond;
-    const moment = new Date(timeInMs * 1000);
-    return moment;
-};
+export const gigasecond = date => new Date((((date.getTime() / 1000) + Math.pow(10, 9)) * 1000));
